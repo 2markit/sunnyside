6745b73b82f1ee5a81a0141b28ec5e2681d1cb20
J <jakemckenzie19@gmail.com>
2017-01-21 12:57:13 -0800

    v1.1.1

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,17 @@ $(function(){
          .end().appendTo('.fadein');},
       3000);
 });
+
+// Scroll to element
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top -75
+      }, 1000);
+      return false;
+    }
+  }
+});
