f7932cafffa3c6ddc31a7b382fab0c78de7b2073
J <jakemckenzie19@gmail.com>
2017-03-07 00:44:29 -0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -7,42 +7,6 @@ $(function(){
       3000);
 });
 
-$(document).ready(function(){
-  $('.collapsible').collapsible();
-});
-
-
-$(document).ready(function(){
-  $('.carousel').carousel();
-  // Next slide
-  $('.carousel').carousel('next');
-  // Move next n times.
-  $('.carousel').carousel('next', 3);
-  // Previous slide
-  $('.carousel').carousel('prev');
-  // Move prev n times.
-  $('.carousel').carousel('prev', 4);
-  // Set to nth slide
-  $('.carousel').carousel('set', 4);
-
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
-});
-
-
-// ===== Scroll to Top ====
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
-
 // Scroll to element
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
