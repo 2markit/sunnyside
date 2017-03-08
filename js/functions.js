911e35d88bbe8777c0584ca724758204c1b758d1
J <jakemckenzie19@gmail.com>
2017-03-07 21:06:03 -0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -7,6 +7,20 @@ $(function(){
       3000);
 });
 
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
 // Scroll to element
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
